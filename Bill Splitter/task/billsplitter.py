83ebc1e547ef1a1051a1abe9e539dfedf86c5103
mjmonarch <mjmonarch@gmail.com>
2021-12-13 22:35:43 +0200

    stage 1 completed

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,1 +1,14 @@
 # write your code here
+# -------------------------STAGE 1---------------------------------------
+friends_dict = {}
+
+print("Enter the number of friends joining (including you):")
+count = int(input())
+print()
+if count < 1:
+    print("No one is joining for the party")
+else:
+    print("Enter the name of every friend (including you), each on a new line:")
+    for i in range(count):
+        friends_dict[input()] = 0
+    print("\n", friends_dict, sep='')
