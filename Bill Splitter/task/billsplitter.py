059a223029183c987f911a1427ec0e21e807a730
mjmonarch <mjmonarch@gmail.com>
2021-12-31 18:07:41 +0200

    stage 3 completed!!!

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -12,7 +12,33 @@
 #         friends_dict[input()] = 0
 #     print("\n", friends_dict, sep='')
 
-# -------------------------STAGE 2---------------------------------------
+# # -------------------------STAGE 2---------------------------------------
+# friends_dict = {}
+#
+# print("Enter the number of friends joining (including you):")
+# count = int(input())
+# print()
+# if count < 1:
+#     print("No one is joining for the party")
+# else:
+#     print("Enter the name of every friend (including you), each on a new line:")
+#     for i in range(count):
+#         friends_dict[input()] = 0
+#     print("Enter the total bill value:")
+#     try:
+#         total = int(input())
+#         per_person = round(total / count, 2)
+#     except ValueError:
+#         print("Non-correct total bill value!")
+#     else:
+#         for friend in friends_dict.keys():
+#             friends_dict[friend] = per_person
+#     finally:
+#         print("\n", friends_dict, sep='')
+
+# -------------------------STAGE 3---------------------------------------
+import random
+
 friends_dict = {}
 
 print("Enter the number of friends joining (including you):")
@@ -27,11 +53,18 @@ else:
     print("Enter the total bill value:")
     try:
         total = int(input())
-        per_person = round(total / count, 2)
+        print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
+        answer = input()
+        if answer == 'Yes':
+            lucky = random.choice(list(friends_dict.keys()))
+            print(f"{lucky} is the lucky one!")
+        else:
+            print("No one is going to be lucky")
+            # per_person = round(total / count, 2)
     except ValueError:
         print("Non-correct total bill value!")
-    else:
-        for friend in friends_dict.keys():
-            friends_dict[friend] = per_person
-    finally:
-        print("\n", friends_dict, sep='')
+    # else:
+    #     for friend in friends_dict.keys():
+    #         friends_dict[friend] = per_person
+    # finally:
+    #     print("\n", friends_dict, sep='')
